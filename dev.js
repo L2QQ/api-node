4b17db4d818c2ec35832e734b711ba308703ec0e
Vitaly Berg <bergusman@gmail.com>
2018-09-13 22:43:54 +0300

    Test api

--- dev.js
+++ dev.js
@@ -1,12 +1,67 @@
 const binance = require('binance')
-const api = new binance.BinanceRest({
-    key: '',
-    secret: ''
+const rest = new binance.BinanceRest({
+    key: '6YcvanPMhtFx2I0VMdU7mDaq6CXO5h04RU2MN2aekS8iTgjRBjT9QrHOOzWHvQhA',
+    secret: 'MJaNpyqEo1dFAnwOIizf86nzPK82qQLegbJ3q5g7IEFIspvpQdIfBB4IBmUYxi2i'
 })
-api._baseUrl = 'http://localhost:9000/'
+rest._baseUrl = 'http://localhost:9000/'
 
-api.account().then((acc) => {
-    console.log(acc)
-}).catch((err) => {
-    console.error(err)
-})
+function account() {
+    rest.account().then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+
+function exchangeInfo() {
+    rest.exchangeInfo().then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+
+function pingAndTime() {
+    rest.ping().then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+
+    rest.time().then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+
+function depth() {
+    rest.depth('ETHUSDT').then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+    
+}
+
+function trades() {
+    rest.trades('ETHUSDT').then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
+
+function newOrder() {
+    rest.newOrder({
+        symbol: 'ETHUSDT',
+        type: 'LIMIT_MAKER',
+        side: 'SELL',
+        price: '20',
+        quantity: '0.5'
+    }).then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
